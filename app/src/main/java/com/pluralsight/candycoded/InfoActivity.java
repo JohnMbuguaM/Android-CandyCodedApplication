1dd091894b96b298c0806f6b1ead1c59221ca6d8
J.M Mbugua <mbuguamuthoni9@gmail.com>
2019-10-29 09:15:26 +0300

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -35,7 +35,12 @@ public class InfoActivity extends AppCompatActivity {
         }
     }
 
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
+    public void createPhoneIntent(View view) {
+        Uri telephoneUri = Uri.parse("tel:0123456789");
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(telephoneUri);
+        startActivity(phoneIntent);
+
+
+    }
 }
